e191383bbca63c82d0fbf16c589fc743b58857ee
Fabio Aiex Alves <fabio.aiex@hotmail.com>
2022-09-30 14:17:12 -0300

    Exercício 1

--- Estudo-de-Java/aula1/src/aula1/Main.java
+++ Estudo-de-Java/aula1/src/aula1/Main.java
@@ -3,8 +3,21 @@ package aula1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int premio = 599;
+		
+		if (premio >= 2401)
+		{
+			System.out.println("Receba o prêmio de R$: 500,00");
+		} else if(premio >= 1801) {
+			System.out.println("Receba o prêmio de R$: 400,00");
+		}else if (premio >= 1201){
+			System.out.println("Receba o prêmio de R$: 300,00");
+		}else if (premio >= 600) {
+			System.out.println("Receba o prêmio de R$: 200,00");
+		}else {
+			System.out.println("Receba o prêmio de R$: 100,00");
+		}
 	}
 
 }
